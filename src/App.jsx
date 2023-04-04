20874aec72b12a368132129929cdbf84e4cb8d74
Brian James Celine <brianceline@gmail.com>
2023-04-05 00:57:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Example1 from './components/Example1'
+
+function App() {
+  return (
+    <Example1 />
+  )
+}
+
+export default App;
---
+++ src/components/Example1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+// import Greetings from './Greetings'
+
+
+//This is another way of showing something on screen without having to import 'Greetings' and we can now delete the file Greetings.jsx from components
+function Greetings(props) {
+  // Everytime we call Greetings, both props name and prof shall be called.
+  // We are destructuring props below and retrieving their vaslues from the Export section below
+  const { name, prof, title } = props;   
+  return (
+    // Below shows how we use props matching what has been used above in the const.
+    // <div>Hello Brian I am a Software Eng.</div>
+    <div> Hello { title } { name } I am a { prof } </div>
+  );
+}
+
+export default function Example1() {
+  return (
+    // <> </> Is known as a fragment
+    <div>
+      <Greetings name = "Brian" prof = "Software Eng." title = "Mr." />
+      <Greetings name = "Janice" prof = "Doctor" title = "Miss."/>
+    </div>
+  );
+}
